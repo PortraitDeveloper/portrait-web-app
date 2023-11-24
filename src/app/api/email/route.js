eddb57f38ee840c18e4dc1b07fb69cebf3a188c7
ValdryanIvandito <valdryan08@gmail.com>
2023-11-24 20:49:23 +0700

    update api/email

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -13,16 +13,20 @@ export async function POST(request) {
   try {
     // Read the body data
     const { email, subject, text } = await request.json();
+    const user = process.env.EMAIL_HOST;
+    const pass = process.env.EMAIL_APP_PASS;
 
-    console.log("Email Parameters:", email, subject, text);
+    console.log("Email Parameters:", email, subject, text, user, pass);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_HOST,
-        pass: process.env.EMAIL_APP_PASS,
+        user: user,
+        pass: pass,
       },
     });
+
+    console.log("Transporter:", transporter);
 
     const mailOptions = {
       from: "noreply",
@@ -31,8 +35,11 @@ export async function POST(request) {
       text: text,
     };
 
+    console.log("mailOptions:", mailOptions);
+
     const info = await transporter.sendMail(mailOptions);
 
+    console.log("Info:", info);
     console.log("Email sent:", info.response);
 
     console.log(currentTimeStamp, "Status: 200, Email sent successfully");
@@ -46,7 +53,10 @@ export async function POST(request) {
       "api/email, Status: 500, An error occurred while processing the request"
     );
     return NextResponse.json(
-      { error: "api/email, Status: 500, An error occurred while processing the request" },
+      {
+        error:
+          "api/email, Status: 500, An error occurred while processing the request",
+      },
       { status: 500 }
     );
   }
