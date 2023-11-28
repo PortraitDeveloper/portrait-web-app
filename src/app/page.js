184178b26f48f360cb108a1b7169685a8ad745ca
ValdryanIvandito <valdryan08@gmail.com>
2023-11-28 19:09:14 +0700

    fixed home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
