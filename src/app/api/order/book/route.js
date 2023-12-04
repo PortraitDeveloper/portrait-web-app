9dc30915e7bf432430c8cf3f5fedde2338e35571
ValdryanIvandito <valdryan08@gmail.com>
2023-12-04 08:23:42 +0700

    fix /api/order/book

--- src/app/api/order/book/route.js
+++ src/app/api/order/book/route.js
@@ -78,116 +78,117 @@ export async function POST(request) {
         },
       });
     }
+    console.log("customer:", customer);
 
-    // Fix product_name string
-    const productNameArray = product_name.toLowerCase().split("  ");
-    const productName = productNameArray[0];
-    // console.log("product_name:", productName);
+    // // Fix product_name string
+    // const productNameArray = product_name.toLowerCase().split("  ");
+    // const productName = productNameArray[0];
+    // // console.log("product_name:", productName);
 
-    // Read product data
-    const product = await prisma.products.findFirst({
-      where: {
-        product_name: productName,
-        branch_id,
-      },
-    });
-    // console.log("product:", product);
+    // // Read product data
+    // const product = await prisma.products.findFirst({
+    //   where: {
+    //     product_name: productName,
+    //     branch_id,
+    //   },
+    // });
+    // // console.log("product:", product);
 
-    // Convert to integer
-    const numberOfAddPerson = parseInt(number_of_add_person);
-    const numberOfAddPets = parseInt(number_of_add_pet);
-    const numberOfAddPrint5R = parseInt(number_of_add_print5r);
+    // // Convert to integer
+    // const numberOfAddPerson = parseInt(number_of_add_person);
+    // const numberOfAddPets = parseInt(number_of_add_pet);
+    // const numberOfAddPrint5R = parseInt(number_of_add_print5r);
 
-    // Convert to boolean
-    let isAddSoftfile = is_add_softfile === "yes";
+    // // Convert to boolean
+    // let isAddSoftfile = is_add_softfile === "yes";
 
-    // Set number of softfile based on boolean value
-    const numberOfAddSoftfile =
-      isAddSoftfile && productName !== "blue room" ? 1 : 0;
+    // // Set number of softfile based on boolean value
+    // const numberOfAddSoftfile =
+    //   isAddSoftfile && productName !== "blue room" ? 1 : 0;
 
-    // If product name is blue room then number of softfile is 0
-    if (productName === "blue room") {
-      isAddSoftfile = false;
-    }
+    // // If product name is blue room then number of softfile is 0
+    // if (productName === "blue room") {
+    //   isAddSoftfile = false;
+    // }
 
-    // Check voucher_code, if empty string then null
-    const voucherCode = voucher_code === "" ? null : voucher_code;
+    // // Check voucher_code, if empty string then null
+    // const voucherCode = voucher_code === "" ? null : voucher_code;
 
-    // Check if voucherCode not null then read voucher data
-    let voucher = null;
-    if (voucherCode) {
-      // Check whether the voucher has been used or not
-      const voucherUsed = await prisma.orders_book.findFirst({
-        where: {
-          cust_id: customer.cust_id,
-          transactions: {
-            voucher_code: voucherCode,
-          },
-        },
-      });
-      // console.log("voucherUsed:", voucherUsed);
+    // // Check if voucherCode not null then read voucher data
+    // let voucher = null;
+    // if (voucherCode) {
+    //   // Check whether the voucher has been used or not
+    //   const voucherUsed = await prisma.orders_book.findFirst({
+    //     where: {
+    //       cust_id: customer.cust_id,
+    //       transactions: {
+    //         voucher_code: voucherCode,
+    //       },
+    //     },
+    //   });
+    //   // console.log("voucherUsed:", voucherUsed);
 
-      // If the voucher has never been used then retrieve the voucher data
-      if (!voucherUsed) {
-        voucher = await prisma.vouchers.findFirst({
-          where: {
-            voucher_code: voucherCode,
-          },
-        });
-      }
-    }
-    // console.log("voucher:", voucher);
+    //   // If the voucher has never been used then retrieve the voucher data
+    //   if (!voucherUsed) {
+    //     voucher = await prisma.vouchers.findFirst({
+    //       where: {
+    //         voucher_code: voucherCode,
+    //       },
+    //     });
+    //   }
+    // }
+    // // console.log("voucher:", voucher);
 
-    // Provides status whether the voucher can be used or not
-    let isVoucherApplied = false;
-    if (voucher) {
-      if (currentTimeStamp < voucher.expired_date && !voucher.is_voucher_used) {
-        isVoucherApplied = true;
-      }
-    }
+    // // Provides status whether the voucher can be used or not
+    // let isVoucherApplied = false;
+    // if (voucher) {
+    //   if (currentTimeStamp < voucher.expired_date && !voucher.is_voucher_used) {
+    //     isVoucherApplied = true;
+    //   }
+    // }
 
-    // Read additionals data
-    const additionals = await prisma.additionals.findMany();
-    // console.log("additionals:", additionals);
+    // // Read additionals data
+    // const additionals = await prisma.additionals.findMany();
+    // // console.log("additionals:", additionals);
 
-    // Determine whether the print5R is color or black&white
-    const foundIndex = productName.indexOf("black&white");
-    const n = foundIndex !== -1 ? 2 : 3;
-    // console.log("n:", n);
+    // // Determine whether the print5R is color or black&white
+    // const foundIndex = productName.indexOf("black&white");
+    // const n = foundIndex !== -1 ? 2 : 3;
+    // // console.log("n:", n);
 
-    // Additionals Price List Calculation
-    const additionalPersonPrice = numberOfAddPerson * additionals[0].item_price;
-    const additionalPetPrice = numberOfAddPets * additionals[1].item_price;
-    const additionalPrint5r = numberOfAddPrint5R * additionals[n].item_price;
-    const additionalSoftfile = numberOfAddSoftfile * additionals[4].item_price;
+    // // Additionals Price List Calculation
+    // const additionalPersonPrice = numberOfAddPerson * additionals[0].item_price;
+    // const additionalPetPrice = numberOfAddPets * additionals[1].item_price;
+    // const additionalPrint5r = numberOfAddPrint5R * additionals[n].item_price;
+    // const additionalSoftfile = numberOfAddSoftfile * additionals[4].item_price;
 
-    // Total price initial
-    let totalPrice = null;
+    // // Total price initial
+    // let totalPrice = null;
 
-    // Total price calculation
-    totalPrice =
-      product.product_price +
-      additionalPersonPrice +
-      additionalPetPrice +
-      additionalPrint5r +
-      additionalSoftfile;
-    // console.log("totalPrice:", totalPrice);
+    // // Total price calculation
+    // totalPrice =
+    //   product.product_price +
+    //   additionalPersonPrice +
+    //   additionalPetPrice +
+    //   additionalPrint5r +
+    //   additionalSoftfile;
+    // // console.log("totalPrice:", totalPrice);
 
-    let totalPaidByCust = totalPrice;
+    // let totalPaidByCust = totalPrice;
 
-    // Check if the voucher can be used and has not been used yet
-    if (isVoucherApplied) {
-      if (voucher.percentage_discount) {
-        // Calculate the total price with a percentage discount
-        totalPaidByCust = totalPrice * (1.0 - voucher.percentage_discount);
-      } else {
-        // Calculate the total price with a nominal discount (if available)
-        totalPaidByCust =
-          totalPrice -
-          (voucher.nominal_discount === null ? 0 : voucher.nominal_discount);
-      }
-    }
-    console.log("totalPaidByCust:", totalPaidByCust);
+    // // Check if the voucher can be used and has not been used yet
+    // if (isVoucherApplied) {
+    //   if (voucher.percentage_discount) {
+    //     // Calculate the total price with a percentage discount
+    //     totalPaidByCust = totalPrice * (1.0 - voucher.percentage_discount);
+    //   } else {
+    //     // Calculate the total price with a nominal discount (if available)
+    //     totalPaidByCust =
+    //       totalPrice -
+    //       (voucher.nominal_discount === null ? 0 : voucher.nominal_discount);
+    //   }
+    // }
+    // console.log("totalPaidByCust:", totalPaidByCust);
 
     // Create transactions data
     // await prisma.transactions.create({
