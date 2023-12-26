63db015f31bcae6e79f9fe7eb58fd03183873e9f
ValdryanIvandito <valdryan08@gmail.com>
2023-12-26 12:54:15 +0700

    update

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -13,7 +13,7 @@ export async function POST(request) {
     });
   }
 
-  const exist = await prisma.credentials.findUnique({
+  const exist = await prisma.User.findUnique({
     where: {
       username,
     },
@@ -27,7 +27,7 @@ export async function POST(request) {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const credential = await prisma.credentials.create({
+  const credential = await prisma.User.create({
     data: {
       username,
       password: hashedPassword,
