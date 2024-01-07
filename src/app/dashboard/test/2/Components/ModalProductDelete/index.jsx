4b6668a060031869b33e2f2aad668c13cd3d198c
ValdryanIvandito <valdryan08@gmail.com>
2024-01-07 23:25:31 +0700

    update

--- src/app/dashboard/test/2/Components/ModalProductDelete/index.jsx
+++ src/app/dashboard/test/2/Components/ModalProductDelete/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import ButtonCancel from "../_ChildComponents/ButtonCancel";
 import ButtonDelete from "../_ChildComponents/ButtonDelete";
@@ -8,10 +9,12 @@ const ModalProductDelete = ({
   closeModal,
   finishModal,
 }) => {
+  const [loading, setLoading] = useState(true);
   let message = null;
   let color = "red";
 
   const deleteHandler = async () => {
+    setLoading(false);
     let response = await fetch("/api/data/product", {
       method: "DELETE",
       headers: {
@@ -22,6 +25,7 @@ const ModalProductDelete = ({
     });
 
     response = await response.json();
+    setLoading(true);
 
     if (response.status !== 200) {
       message = `Gagal menghapus product ${productData.productName}`;
@@ -41,34 +45,57 @@ const ModalProductDelete = ({
   if (!isVisible) return null;
 
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
-      id="container"
-      onClick={closeHandler}
-    >
-      <div className="bg-white p-6 rounded-2xl">
-        <div className="flex justify-center items-center mb-6">
-          <Image src="/warning.png" alt="Warning Sign" width={40} height={40} />
-        </div>
+    <>
+      {loading && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
+          id="container"
+          onClick={closeHandler}
+        >
+          <div className="bg-white p-6 rounded-2xl">
+            <div className="flex justify-center items-center mb-6">
+              <Image
+                src="/warning.png"
+                alt="Warning Sign"
+                width={40}
+                height={40}
+              />
+            </div>
 
-        <div className="text-center mb-5">
-          <div className="text-red-500 font-bold mb-3">Apakah kamu yakin</div>
-          <div className="text-sm">Ingin menghapus produk</div>
-          <div className="text-sm font-bold w-56">
-            {productData.productName}
+            <div className="text-center mb-5">
+              <div className="text-red-500 font-bold mb-3">
+                Apakah kamu yakin
+              </div>
+              <div className="text-sm">Ingin menghapus produk</div>
+              <div className="text-sm font-bold w-56">
+                {productData.productName}
+              </div>
+            </div>
+
+            <div className="flex justify-center gap-3">
+              <ButtonCancel
+                getCancel={() => {
+                  closeModal();
+                }}
+              />
+              <ButtonDelete getDelete={deleteHandler} />
+            </div>
           </div>
         </div>
+      )}
 
-        <div className="flex justify-center gap-3">
-          <ButtonCancel
-            getCancel={() => {
-              closeModal();
-            }}
-          />
-          <ButtonDelete getDelete={deleteHandler} />
+      {!loading && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
+          id="container"
+          onClick={closeHandler}
+        >
+          <div className="bg-white p-6 rounded-2xl text-center">
+            <p>Process...</p>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
